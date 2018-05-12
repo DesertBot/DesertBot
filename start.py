81b6a138c476084f9ddd6063f31d3efd0ba6e2cf
Heufneutje <stefan.frijters@gmail.com>
2018-05-12 09:11:12 +0000

    Make the logging level configurable

--- start.py
+++ start.py
@@ -15,6 +15,9 @@ if __name__ == '__main__':
     parser.add_argument('-c', '--config',
                         help='the config file to read from',
                         type=str, required=True)
+    parser.add_argument('-l', '--loglevel',
+                        help='the logging level (default INFO)',
+                        type=str, default='INFO')
     cmdArgs = parser.parse_args()
 
     os.chdir(os.path.dirname(os.path.abspath(__file__)))
@@ -22,7 +25,11 @@ if __name__ == '__main__':
     # Set up logging for stdout on the root 'desertbot' logger
     # Modules can then just add more handlers to the root logger to capture all logs to files in various ways
     rootLogger = logging.getLogger('desertbot')
-    rootLogger.setLevel(logging.INFO)  # TODO change this from config value once it's loaded
+    numericLevel = getattr(logging, cmdArgs.loglevel.upper(), None)
+    if isinstance(numericLevel, int):
+        rootLogger.setLevel(numericLevel)
+    else:
+        raise ValueError('Invalid log level {}'.format(cmdArgs.loglevel))
 
     logFormatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s', '%H:%M:%S')
 
