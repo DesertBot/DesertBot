0d9a251a5edc12ddb5398fbf7e7d2639b552c58a
Heufneutje <stefan.frijters@gmail.com>
2018-05-27 23:35:44 +0200

    [Pronouns] Switch to using the context manager

--- desertbot/modules/commands/Pronouns.py
+++ desertbot/modules/commands/Pronouns.py
@@ -2,7 +2,7 @@ from desertbot.datastore import Base
 from sqlalchemy import Column, Integer, String
 
 from twisted.plugin import IPlugin
-from desertbot.datastore import Session
+from desertbot.datastore import sessionScope
 from desertbot.moduleinterface import IModule
 from desertbot.modules.commandinterface import BotCommand
 from zope.interface import implementer
@@ -35,41 +35,33 @@ class Pronouns(BotCommand):
         Base.metadata.remove(Pronoun.__table__)
 
     def execute(self, message: IRCMessage):
-        session = Session()
-
-        if message.command == "setpron":
-            if len(message.parameterList) < 1:
-                return IRCResponse(ResponseType.Say, "Your pronouns are... blank?", message.replyTo)
-            userPronouns = Pronoun(nick=message.user.nick.lower(), pronouns=message.parameters)
-            session.add(userPronouns)
-            session.commit()
-            session.close()
-            return IRCResponse(ResponseType.Say, "Your pronouns have been set as <{}>.".format(message.parameters),
-                               message.replyTo)
-        elif message.command == "rmpron":
-            userPronouns = session.query(Pronoun).filter(Pronoun.nick == message.user.nick.lower()).first()
-            if userPronouns is None:
-                session.close()
-                return IRCResponse(ResponseType.Say, "I don't even know your pronouns!", message.replyTo)
-            else:
-                session.delete(userPronouns)
-                session.commit()
-                session.close()
-                return IRCResponse(ResponseType.Say, "Your pronouns have been deleted.", message.replyTo)
-        elif message.command == "pronouns":
-            if len(message.parameterList) < 1:
-                lookup = message.user.nick
-            else:
-                lookup = message.parameterList[0]
-
-            user = session.query(Pronoun).filter(Pronoun.nick == lookup.lower()).first()
-            if user is None:
-                session.close()
-                return IRCResponse(ResponseType.Say, "User's pronouns have not been specified.", message.replyTo)
-            else:
-                session.close()
-                return IRCResponse(ResponseType.Say, "{} uses <{}> pronouns.".format(lookup, str(user.pronouns)),
+        with sessionScope() as session:
+            if message.command == "setpron":
+                if len(message.parameterList) < 1:
+                    return IRCResponse(ResponseType.Say, "Your pronouns are... blank?", message.replyTo)
+                userPronouns = Pronoun(nick=message.user.nick.lower(), pronouns=message.parameters)
+                session.add(userPronouns)
+                return IRCResponse(ResponseType.Say, "Your pronouns have been set as <{}>.".format(message.parameters),
                                    message.replyTo)
+            elif message.command == "rmpron":
+                userPronouns = session.query(Pronoun).filter(Pronoun.nick == message.user.nick.lower()).first()
+                if userPronouns is None:
+                    return IRCResponse(ResponseType.Say, "I don't even know your pronouns!", message.replyTo)
+                else:
+                    session.delete(userPronouns)
+                    return IRCResponse(ResponseType.Say, "Your pronouns have been deleted.", message.replyTo)
+            elif message.command == "pronouns":
+                if len(message.parameterList) < 1:
+                    lookup = message.user.nick
+                else:
+                    lookup = message.parameterList[0]
+
+                user = session.query(Pronoun).filter(Pronoun.nick == lookup.lower()).first()
+                if user is None:
+                    return IRCResponse(ResponseType.Say, "User's pronouns have not been specified.", message.replyTo)
+                else:
+                    return IRCResponse(ResponseType.Say, "{} uses <{}> pronouns.".format(lookup, str(user.pronouns)),
+                                       message.replyTo)
 
 
 pronouns = Pronouns()
