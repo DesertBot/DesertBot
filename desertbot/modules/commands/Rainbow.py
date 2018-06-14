5ea202af00ea41d21a2bbaad2feba7ca4c284a2e
StarlitGhost <679547+StarlitGhost@users.noreply.github.com>
2018-06-14 18:16:16 +0100

    [Rainbow] add ability to specify colour number list

--- desertbot/modules/commands/Rainbow.py
+++ desertbot/modules/commands/Rainbow.py
@@ -23,36 +23,67 @@ class Rainbow(BotCommand):
     def help(self, query):
         return 'rainbow <text> - outputs the specified text with rainbow colours; rrainbow uses background colours'
 
-    colours = [assembleFormattedText(A.fg.lightRed['']),
-               #assembleFormattedText(A.fg.orange['']),
+    colours = [assembleFormattedText(A.fg.white['']),
+               assembleFormattedText(A.fg.black['']),
+               assembleFormattedText(A.fg.blue['']),
+               assembleFormattedText(A.fg.green['']),
+               assembleFormattedText(A.fg.lightRed['']),
+               assembleFormattedText(A.fg.red['']),
+               assembleFormattedText(A.fg.magenta['']),
+               assembleFormattedText(A.fg.orange['']),
                assembleFormattedText(A.fg.yellow['']),
                assembleFormattedText(A.fg.lightGreen['']),
+               assembleFormattedText(A.fg.cyan['']),
                assembleFormattedText(A.fg.lightCyan['']),
                assembleFormattedText(A.fg.lightBlue['']),
                assembleFormattedText(A.fg.lightMagenta['']),
+               assembleFormattedText(A.fg.gray['']),
+               assembleFormattedText(A.fg.lightGray['']),
                ]
     
-    bgcolours = [assembleFormattedText(A.bg.red['']),
+    bgcolours = [assembleFormattedText(A.bg.white['']),
+                 assembleFormattedText(A.bg.black['']),
+                 assembleFormattedText(A.bg.blue['']),
+                 assembleFormattedText(A.bg.green['']),
+                 assembleFormattedText(A.bg.lightRed['']),
+                 assembleFormattedText(A.bg.red['']),
+                 assembleFormattedText(A.bg.magenta['']),
                  assembleFormattedText(A.bg.orange['']),
                  assembleFormattedText(A.bg.yellow['']),
-                 assembleFormattedText(A.bg.green['']),
+                 assembleFormattedText(A.bg.lightGreen['']),
                  assembleFormattedText(A.bg.cyan['']),
-                 assembleFormattedText(A.bg.blue['']),
-                 assembleFormattedText(A.bg.magenta['']),
+                 assembleFormattedText(A.bg.lightCyan['']),
+                 assembleFormattedText(A.bg.lightBlue['']),
+                 assembleFormattedText(A.bg.lightMagenta['']),
+                 assembleFormattedText(A.bg.gray['']),
+                 assembleFormattedText(A.bg.lightGray['']),
                  ]
 
     def execute(self, message: IRCMessage):
         if len(message.parameterList) == 0:
             return IRCResponse(ResponseType.Say, "You didn't give me any text to rainbow!", message.replyTo)
+        
+        if message.command == 'rainbow':
+            fg = True
+        else:
+            fg = False
+
+        try:
+            colList = [int(n) for n in message.parameterList[0].split(',')]
+        except ValueError:
+            if fg:
+                colList = [4,8,9,11,12,13]
+            else:
+                colList = [5,7,8,3,10,2,6]
 
         outputMessage = u''
 
-        if message.command == 'rainbow':
+        if fg:
             for i, c in enumerate(message.parameters):
-                outputMessage += self.colours[i % len(self.colours)] + c
+                outputMessage += self.colours[colList[i % len(colList)]] + c
         else:
             for i, c in enumerate(message.parameters):
-                outputMessage += self.bgcolours[i % len(self.bgcolours)] + c
+                outputMessage += self.bgcolours[colList[i % len(colList)]] + c
 
         outputMessage += assembleFormattedText(A.normal[''])
 
