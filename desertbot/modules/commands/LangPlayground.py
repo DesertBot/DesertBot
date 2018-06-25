cd0a17ca4c01441ff931966cd4f38158626506d3
StarlitGhost <679547+StarlitGhost@users.noreply.github.com>
2018-06-25 15:47:35 +0100

    [Lang] add langurl to run code on tio via url. Progresses #55

--- desertbot/modules/commands/LangPlayground.py
+++ desertbot/modules/commands/LangPlayground.py
@@ -12,6 +12,7 @@ from zope.interface import implementer
 from typing import List
 
 import zlib
+from collections import OrderedDict
 
 import requests
 
@@ -21,7 +22,7 @@ from desertbot.response import IRCResponse, ResponseType
 @implementer(IPlugin, IModule)
 class LangPlayground(BotCommand):
     def triggers(self):
-        return ['lang']
+        return self.commands.keys()
 
     def help(self, query: List[str]) -> str:
         return self._helpText()
@@ -50,7 +51,7 @@ int main() {{
     def _helpText(self):
         return u"{}lang <lang> <code> - evaluates the given code using https://tio.run".format(self.bot.commandChar)
 
-    def _tio(self, lang: str, code: str) -> str:
+    def _tio(self, lang: str, code: str, userInput: str="") -> str:
         if self.languages == None:
             langUrl = "https://raw.githubusercontent.com/TryItOnline/tryitonline/master/usr/share/tio.run/languages.json"
             response = requests.get(langUrl)
@@ -66,11 +67,9 @@ int main() {{
             return "[Language {!r} unknown on tio.run. Perhaps you want: {}]".format(lang,
                                                                                      langString)
 
-        if lang in self.templates:
-            code = self.templates[lang].format(code=code)
-
         request = [{'command': 'V', 'payload': {'lang': [lang]}},
                    {'command': 'F', 'payload': {'.code.tio': code}},
+                   {'command': 'F', 'payload': {'.input.tio': userInput}},
                    {'command': 'RC'}]
         req = b''
         for instr in request:
@@ -96,9 +95,20 @@ int main() {{
         errors = errors[0].decode('utf-8', 'ignore')
         # Grab and check the exit code
         if int(errors.splitlines()[-1][len("Exit code: ")-1:]) != 0:
-            paste = "{code}\n\n/* --- stdout ---\n{stdout}\n*/\n\n/* --- stderr ---\n{stderr}\n*/".format(code=code, stdout=returned[0].decode('utf-8', 'ignore'), stderr=errors)
+            paste = ("{code}\n"
+                     "\n"
+                     "/* --- stdout ---\n"
+                     "{stdout}\n"
+                     "*/\n"
+                     "\n"
+                     "/* --- stderr ---\n"
+                     "{stderr}\n"
+                     "*/".format(code=code,
+                                 stdout=returned[0].decode('utf-8', 'ignore'),
+                                 stderr=errors))
             url = self.bot.moduleHandler.runActionUntilValue('upload-pasteee',
-                                                             paste, "TIO stderr", 10)
+                                                             paste,
+                                                             "TIO stderr", 10)
             error = "Errors occurred! Output: {url}".format(url=url)
             if lang in self.templates:
                 error += " (language uses a template, see link for framing code)"
@@ -106,14 +116,43 @@ int main() {{
 
         return u' | '.join(r.decode('utf-8', 'ignore') for r in returned)
 
-    def execute(self, message: IRCMessage):
+    def _lang(self, message: IRCMessage):
         if len(message.parameterList) > 0:
             lang = message.parameterList[0].lower()
-            result = self._tio(lang, u' '.join(message.parameterList[1:]))
+            code = u' '.join(message.parameterList[1:])
+            if lang in self.templates:
+                code = self.templates[lang].format(code=code)
+            result = self._tio(lang, code)
+            return IRCResponse(ResponseType.Say,
+                               result.replace("\n", " "),
+                               message.replyTo)
         else:
-            return IRCResponse(ResponseType.Say, self._helpText(), message.replyTo)
+            return IRCResponse(ResponseType.Say,
+                               self._helpText(),
+                               message.replyTo)
 
-        return IRCResponse(ResponseType.Say, result.replace("\n", " "), message.replyTo)
+    def _langurl(self, message: IRCMessage):
+        if len(message.parameterList) > 1:
+            lang = message.parameterList[0].lower()
+            url = message.parameterList[1]
+            userInput = " ".join(message.parameterList[2:])
+            page = self.bot.moduleHandler.runActionUntilValue('fetch-url', url)
+            code = page.body
+            result = self._tio(lang, code, userInput)
+            return IRCResponse(ResponseType.Say,
+                               result.replace("\n", " "),
+                               message.replyTo)
+        else:
+            return IRCResponse(ResponseType.Say,
+                               self._helpText(),
+                               message.replyTo)
+
+    commands = OrderedDict([
+        (u'lang', _lang),
+        (u'langurl', _langurl)])
+
+    def execute(self, message: IRCMessage):
+        return self.commands[message.command.lower()](self, message)
 
 
 langPlayground = LangPlayground()
