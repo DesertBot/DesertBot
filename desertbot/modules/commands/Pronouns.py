91be20cfd6467cf32b5afb90e4d70ea522a57f18
HubbeKing <hubbe128@gmail.com>
2018-08-28 18:38:24 +0300

    Remove SQLAlchemy ORM and use shelve instead

--- desertbot/modules/commands/Pronouns.py
+++ desertbot/modules/commands/Pronouns.py
@@ -1,22 +1,10 @@
-from desertbot.datastore import Base
-from sqlalchemy import Column, Integer, String
-
 from twisted.plugin import IPlugin
-from desertbot.datastore import sessionScope
 from desertbot.moduleinterface import IModule
 from desertbot.modules.commandinterface import BotCommand
 from zope.interface import implementer
 
 from desertbot.message import IRCMessage
 from desertbot.response import IRCResponse, ResponseType
-
-
-class Pronoun(Base):
-    __tablename__ = "pronouns"
-
-    id = Column(Integer, primary_key=True)
-    nick = Column(String)
-    pronouns = Column(String)
 
 
 @implementer(IPlugin, IModule)
@@ -29,43 +17,41 @@ class Pronouns(BotCommand):
                "Query the user's pronouns, specify your own pronouns, or remove your pronouns from the database."
 
     def onLoad(self):
-        Base.metadata.create_all(self.bot.databaseEngine)
-
-    def onUnload(self):
-        Base.metadata.remove(Pronoun.__table__)
+        if "pronouns" not in self.bot.storage or not type(self.bot.storage["pronouns"] == dict):
+            self.bot.storage["pronouns"] = {}
+        self.pronounStorage = self.bot.storage["pronouns"]
 
     def execute(self, message: IRCMessage):
-        with sessionScope() as session:
-            if message.command == "setpron":
-                if len(message.parameterList) < 1:
-                    return IRCResponse(ResponseType.Say, "Your pronouns are... blank?", message.replyTo)
-                userPronouns = session.query(Pronoun).filter(Pronoun.nick == message.user.nick.lower()).first()
-                if userPronouns is not None:
-                    userPronouns.pronouns = message.parameters
-                else:
-                    userPronouns = Pronoun(nick=message.user.nick.lower(), pronouns=message.parameters)
-                    session.add(userPronouns)
-                return IRCResponse(ResponseType.Say, "Your pronouns have been set as <{}>.".format(message.parameters),
-                                   message.replyTo)
-            elif message.command == "rmpron":
-                userPronouns = session.query(Pronoun).filter(Pronoun.nick == message.user.nick.lower()).first()
-                if userPronouns is None:
-                    return IRCResponse(ResponseType.Say, "I don't even know your pronouns!", message.replyTo)
-                else:
-                    session.delete(userPronouns)
-                    return IRCResponse(ResponseType.Say, "Your pronouns have been deleted.", message.replyTo)
-            elif message.command == "pronouns":
-                if len(message.parameterList) < 1:
-                    lookup = message.user.nick
-                else:
-                    lookup = message.parameterList[0]
+        if message.command == "setpron":
+            if len(message.parameterList) < 1:
+                return IRCResponse(ResponseType.Say, "Your pronouns are... blank?", message.replyTo)
 
-                user = session.query(Pronoun).filter(Pronoun.nick == lookup.lower()).first()
-                if user is None:
-                    return IRCResponse(ResponseType.Say, "User's pronouns have not been specified.", message.replyTo)
-                else:
-                    return IRCResponse(ResponseType.Say, "{} uses <{}> pronouns.".format(lookup, str(user.pronouns)),
-                                       message.replyTo)
+            self.pronounStorage[message.user.nick.lower()] = message.parameters
+            self.bot.storage["pronouns"] = self.pronounStorage
+
+            return IRCResponse(ResponseType.Say, "Your pronouns have been set as <{}>.".format(message.parameters),
+                               message.replyTo)
+
+        elif message.command == "rmpron":
+            if message.user.nick.lower() not in self.pronounStorage:
+                return IRCResponse(ResponseType.Say, "I don't even know your pronouns!", message.replyTo)
+            else:
+                del self.pronounStorage[message.user.nick.lower()]
+                self.bot.storage["pronouns"] = self.pronounStorage
+
+                return IRCResponse(ResponseType.Say, "Your pronouns have been deleted.", message.replyTo)
+        elif message.command == "pronouns":
+            if len(message.parameterList) < 1:
+                lookup = message.user.nick
+            else:
+                lookup = message.parameterList[0]
+
+            if lookup.lower() not in self.pronounStorage:
+                return IRCResponse(ResponseType.Say, "User's pronouns have not been specified.", message.replyTo)
+            else:
+                return IRCResponse(ResponseType.Say,
+                                   "{} uses <{}> pronouns.".format(lookup, self.pronounStorage[lookup.lower()]),
+                                   message.replyTo)
 
 
 pronouns = Pronouns()
