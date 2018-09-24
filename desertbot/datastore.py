f17310e0fcf5d7ea7ceab2b9243f106eb1222b69
HubbeKing <hubbe128@gmail.com>
2018-09-24 18:43:43 +0300

    Allow using DataStore class as if dict

--- desertbot/datastore.py
+++ desertbot/datastore.py
@@ -5,12 +5,11 @@ import os
 class DataStore(object):
     def __init__(self, storagePath="desertbot_data.json"):
         self.storagePath = storagePath
-        self.data = None
+        self.data = {}
         self.load()
 
     def load(self):
         if not os.path.exists(self.storagePath):
-            self.data = {}
             self.save()
             return
         with open(self.storagePath) as storageFile:
@@ -21,3 +20,12 @@ class DataStore(object):
         with open(tmpFile, "w") as storageFile:
             storageFile.write(json.dumps(self.data, indent=4))
         os.rename(tmpFile, self.storagePath)
+
+    def __len__(self):
+        return len(self.data)
+
+    def __iter__(self):
+        return iter(self.data)
+
+    def __getitem__(self, item):
+        return self.data[item]
