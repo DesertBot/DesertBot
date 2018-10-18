026f6c34d988cbd52a81902121ef02ac899cf1fb
StarlitGhost <679547+StarlitGhost@users.noreply.github.com>
2018-10-18 17:38:03 +0100

    [Imgur] add handling for subreddit gallery image titles, and fix the html fallback by using the original link ID (gallery/ / a/ included) (fixes #86)

--- desertbot/modules/urlfollow/Imgur.py
+++ desertbot/modules/urlfollow/Imgur.py
@@ -27,34 +27,31 @@ class Imgur(BotCommand):
     def onLoad(self):
         self.imgurClientID = load_key('imgur Client ID')
 
-    def follow(self, _: IRCMessage, url: str) -> [str, None]:
-        match = re.search(r'(i\.)?imgur\.com/(?P<imgurID>[^\.]+)', url)
+    def follow(self, _: IRCMessage, origUrl: str) -> [str, None]:
+        match = re.search(r'(i\.)?imgur\.com/(?P<imgurID>[^\.]+)', origUrl)
         if not match:
             return
-        imgurID = match.group('imgurID')
+        origImgurID = match.group('imgurID')
 
         if self.imgurClientID is None:
             return '[imgur Client ID not found]', None
 
-        if imgurID.startswith('gallery/'):
-            imgurID = imgurID.replace('gallery/', '')
-
         albumLink = False
-        if imgurID.startswith('a/'):
-            imgurID = imgurID.replace('a/', '')
-            url = 'https://api.imgur.com/3/album/{0}'.format(imgurID)
+        if origImgurID.startswith('a/'):
+            imgurID = origImgurID.replace('a/', '')
+            url = 'https://api.imgur.com/3/album/{}'.format(imgurID)
             albumLink = True
+        elif origImgurID.startswith('gallery/'):
+            imgurID = origImgurID.replace('gallery/', '')
+            url = 'https://api.imgur.com/3/gallery/{}'.format(imgurID)
         else:
-            url = 'https://api.imgur.com/3/image/{0}'.format(imgurID)
+            imgurID = origImgurID
+            url = 'https://api.imgur.com/3/image/{}'.format(origImgurID)
 
-        headers = {'Authorization': 'Client-ID {0}'.format(self.imgurClientID)}
+        headers = {'Authorization': 'Client-ID {}'.format(self.imgurClientID)}
 
         mh = self.bot.moduleHandler
         response = mh.runActionUntilValue('fetch-url', url, extraHeaders=headers)
-
-        if not response:
-            url = 'https://api.imgur.com/3/gallery/{0}'.format(imgurID)
-            response = mh.runActionUntilValue('fetch-url', url, extraHeaders=headers)
 
         if not response:
             return
@@ -63,17 +60,24 @@ class Imgur(BotCommand):
 
         imageData = j['data']
 
-        if imageData['title'] is None:
-            url = 'https://api.imgur.com/3/gallery/{0}'.format(imgurID)
-            response = mh.runActionUntilValue('fetch-url', url, extraHeaders=headers)
-            if response:
+        if not imageData['title']:
+            if imageData['section']:
+                # subreddit galleries have a different endpoint with better data.
+                #  we don't know if it's a subreddit gallery image until we fetch it,
+                #  so we're stuck with this double-lookup. oh well.
+                url = ('https://api.imgur.com/3/gallery/r/{}/{}'
+                       .format(imageData['section'], imgurID))
+                response = mh.runActionUntilValue('fetch-url', url, extraHeaders=headers)
+                if not response:
+                    return
                 j = response.json()
-                if j['success']:
-                    imageData = j['data']
-
-            if imageData['title'] is None:
-                url = 'http://imgur.com/{0}'.format(imgurID)
-                response = mh.runActionUntilValue('fetch-url', url)
+                imageData = j['data']
+            else:
+                # fallback to the html page title if no other title was found.
+                #  this should always result in <No Title> now as all the endpoints are covered,
+                #  but they may add new ones so we're leaving this here just in case.
+                titleUrl = 'https://imgur.com/{}'.format(origImgurID)
+                response = mh.runActionUntilValue('fetch-url', titleUrl)
                 title = mh.runActionUntilValue('get-html-title', response.content)
                 imageData['title'] = title.replace(' - Imgur', '')
                 if imageData['title'] in ['imgur: the simple image sharer',
@@ -81,23 +85,23 @@ class Imgur(BotCommand):
                     imageData['title'] = None
 
         data = []
-        if imageData['title'] is not None:
+        if imageData['title']:
             data.append(imageData['title'])
         else:
             data.append('<No Title>')
         if imageData['nsfw']:
             data.append('\x034\x02NSFW!\x0F')
         if albumLink:
-            data.append('Album: {0} Images'.format(imageData['images_count']))
+            data.append('Album: {} Images'.format(imageData['images_count']))
         else:
             if 'is_album' in imageData and imageData['is_album']:
-                data.append('Album: {0:,d} Images'.format(len(imageData['images'])))
+                data.append('Album: {:,d} Images'.format(len(imageData['images'])))
             else:
                 if imageData['animated']:
                     data.append('\x032\x02Animated!\x0F')
-                data.append('{0:,d}x{1:,d}'.format(imageData['width'], imageData['height']))
-                data.append('Size: {0:,d}kb'.format(int(imageData['size']/1024)))
-        data.append('Views: {0:,d}'.format(imageData['views']))
+                data.append('{:,d}x{:,d}'.format(imageData['width'], imageData['height']))
+                data.append('Size: {:,d}kb'.format(int(imageData['size']/1024)))
+        data.append('Views: {:,d}'.format(imageData['views']))
 
         graySplitter = colour(A.normal[' ', A.fg.gray['|'], ' '])
         return graySplitter.join(data), '[no imgur url]'
