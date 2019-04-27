d83479baf73596d5997d2dd70f9a4a55a9501c9d
Heufneutje <stefan.frijters@gmail.com>
2019-04-28 01:01:57 +0200

    Port PyHeufyBot's commands command

---
+++ desertbot/modules/commands/Commands.py
@@ -0,0 +1,28 @@
+from twisted.plugin import IPlugin
+from desertbot.message import IRCMessage
+from desertbot.response import IRCResponse, ResponseType
+from desertbot.moduleinterface import IModule
+from desertbot.modules.commandinterface import BotCommand
+from typing import Union
+from zope.interface import implementer
+
+
+@implementer(IPlugin, IModule)
+class Commands(BotCommand):
+    def triggers(self):
+        return ["commands"]
+
+    def help(self, query: Union[str, None]) -> str:
+        return "Commands: commands - Lists all bot commands from all loaded modules."
+
+    def execute(self, message: IRCMessage):
+        commandsList = []
+        for moduleName, module in self.bot.moduleHandler.modules.items():
+            if isinstance(module, BotCommand):
+                commandsList += module.triggers()
+        return IRCResponse(ResponseType.Say,
+                           "Available commands: {}".format(", ".join(sorted(commandsList))),
+                           message.replyTo)
+
+
+commandsCommand = Commands()
