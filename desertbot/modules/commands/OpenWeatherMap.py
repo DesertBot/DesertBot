9a0355ec92187756d7f33d9e8009880d4f817242
Heufneutje <stefan.frijters@gmail.com>
2019-04-28 08:02:41 +0000

    [OpenWeatherMap] Fix typo in help text

--- desertbot/modules/commands/OpenWeatherMap.py
+++ desertbot/modules/commands/OpenWeatherMap.py
@@ -19,7 +19,7 @@ class OpenWeatherMap(BotCommand):
     def help(self, arg):
         return "Commands: weather/forecast (<latlon/user/place>) - Uses the OpenWeatherMap API to request the weather " \
                "for a given set of coordinates, username or place. Requests the users own weather or forecast when no " \
-               "paramters are givne."
+               "paramters are given."
 
     def onLoad(self):
         self.apiKey = load_key("OpenWeatherMap")
