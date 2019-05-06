f544d29c6013e8a346b63256a29bf7edf78f2b7a
Heufneutje <stefan.frijters@gmail.com>
2019-05-06 08:42:03 +0000

    [Log] Make logging format consistent with PyHeufyBot's for easier porting

--- desertbot/modules/automatic/Log.py
+++ desertbot/modules/automatic/Log.py
@@ -13,28 +13,44 @@ import datetime
 import codecs
 import os
 
-from desertbot.message import IRCMessage
+from desertbot.message import IRCMessage, TargetTypes
 from desertbot.response import IRCResponse, ResponseType
 
+
 logFuncs = {
-    'PRIVMSG': lambda m: '<{0}> {1}'.format(m.user.nick, m.messageString),
+    'PRIVMSG': lambda m: formatPrivmsg(m),
     'ACTION': lambda m: '*{0} {1}*'.format(m.user.nick, m.messageString),
     'NOTICE': lambda m: '[{0}] {1}'.format(m.user.nick, m.messageString),
-    'JOIN': lambda m: ' >> {0} ({1}@{2}) joined {3}'.format(m.user.nick, m.user.ident, m.user.host, m.replyTo),
+    'JOIN': lambda m: '>> {0} ({1}@{2}) joined {3}'.format(m.user.nick, m.user.ident, m.user.host, m.replyTo),
     'NICK': lambda m: '{0} is now known as {1}'.format(m.user.nick, m.messageString),
-    'PART': lambda m: ' << {0} ({1}@{2}) left {3}{4}'.format(m.user.nick, m.user.ident, m.user.host, m.replyTo, ': ' + m.messageString if m.messageString else ''),
-    'QUIT': lambda m: ' << {0} ({1}@{2}) quit{3}'.format(m.user.nick, m.user.ident, m.user.host, ': '+m.messageString if m.messageString else ''),
-    'KICK': lambda m: '!<< {0} was kicked by {1}{2}'.format(m.metadata['kicked'], m.user.nick, ': '+m.messageString if m.messageString else ''),
-    'TOPIC': lambda m: '# {0} set the topic to: {1}'.format(m.user.nick, m.messageString),
+    'PART': lambda m: '<< {0} ({1}@{2}) left {3}{4}'.format(m.user.nick, m.user.ident, m.user.host, m.replyTo, ': ' + m.messageString if m.messageString else ''),
+    'QUIT': lambda m: '<< {0} ({1}@{2}) quit{3}'.format(m.user.nick, m.user.ident, m.user.host, ': '+m.messageString if m.messageString else ''),
+    'KICK': lambda m: '-- {0} was kicked by {1}{2}'.format(m.metadata['kicked'], m.user.nick, ': '+m.messageString if m.messageString else ''),
+    'TOPIC': lambda m: '-- {0} set the topic to: {1}'.format(m.user.nick, m.messageString),
     'MODE': lambda m: formatMode(m),
 }
 
 logSelfFuncs = {
-    ResponseType.Say: lambda nick, r: '<{0}> {1}'.format(nick, r.response),
-    ResponseType.Do: lambda nick, r: '*{0} {1}*'.format(nick, r.response),
-    ResponseType.Notice: lambda nick, r: '[{0}] {1}'.format(nick, r.response),
+    ResponseType.Say: lambda bot, r: formatSelfPrivmsg(bot, r),
+    ResponseType.Do: lambda bot, r: '*{0} {1}*'.format(bot.nick, r.response),
+    ResponseType.Notice: lambda bot, r: '[{0}] {1}'.format(bot.nick, r.response),
 }
 
+def formatSelfPrivmsg(bot, response):
+    if bot.nick in bot.users and response.target in bot.channels:
+        status = bot.channels[response.target].getHighestStatusOfUser(bot.users[bot.nick])
+    else:
+        status = ''
+
+    return '<{0}{1}> {2}'.format(status, bot.nick, response.response)
+
+def formatPrivmsg(msg: IRCMessage):
+    if msg.targetType == TargetTypes.CHANNEL:
+        status = msg.channel.getHighestStatusOfUser(msg.user)
+    else:
+        status = ''
+
+    return '<{0}{1}> {2}'.format(status, msg.user.nick, msg.messageString)
 
 def formatMode(msg: IRCMessage):
     added = msg.metadata['added']
@@ -57,12 +73,12 @@ def formatMode(msg: IRCMessage):
         if len(removedParams) > 0:
             modeStr += ' {}'.format(' '.join(removedParams))
 
-    return '# {} sets mode: {}'.format(msg.user.nick, modeStr)
+    return '-- {} sets mode: {}'.format(msg.user.nick, modeStr)
 
 
 def log(path, target, text):
     now = datetime.datetime.utcnow()
-    time = now.strftime("[%H:%M]")
+    time = now.strftime("[%H:%M:%S]")
     data = '{0} {1}'.format(time, text)
     print(target, data)
 
@@ -115,7 +131,7 @@ class Log(BotCommand):
 
     def output(self, response: IRCResponse):
         if response.type in logSelfFuncs:
-            logString = logSelfFuncs[response.type](self.bot.nick, response)
+            logString = logSelfFuncs[response.type](self.bot, response)
             log(os.path.join(self.bot.logPath, self.bot.server),
                 response.target,
                 logString)
