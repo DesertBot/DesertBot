652b775a5ac71c53f0b4bf8200d9a409739142df
StarlitGhost <679547+StarlitGhost@users.noreply.github.com>
2019-05-08 11:40:30 +0100

    [Log] #channel-date.txt -> #channel/date.txt. Also replace .formats with f-strings where it makes sense

--- desertbot/modules/automatic/Log.py
+++ desertbot/modules/automatic/Log.py
@@ -19,21 +19,21 @@ from desertbot.response import IRCResponse, ResponseType
 
 logFuncs = {
     'PRIVMSG': lambda m: formatPrivmsg(m),
-    'ACTION': lambda m: '*{0} {1}*'.format(m.user.nick, m.messageString),
-    'NOTICE': lambda m: '[{0}] {1}'.format(m.user.nick, m.messageString),
-    'JOIN': lambda m: '>> {0} ({1}@{2}) joined {3}'.format(m.user.nick, m.user.ident, m.user.host, m.replyTo),
-    'NICK': lambda m: '{0} is now known as {1}'.format(m.user.nick, m.messageString),
-    'PART': lambda m: '<< {0} ({1}@{2}) left {3}{4}'.format(m.user.nick, m.user.ident, m.user.host, m.replyTo, ': ' + m.messageString if m.messageString else ''),
-    'QUIT': lambda m: '<< {0} ({1}@{2}) quit{3}'.format(m.user.nick, m.user.ident, m.user.host, ': '+m.messageString if m.messageString else ''),
-    'KICK': lambda m: '-- {0} was kicked by {1}{2}'.format(m.metadata['kicked'], m.user.nick, ': '+m.messageString if m.messageString else ''),
-    'TOPIC': lambda m: '-- {0} set the topic to: {1}'.format(m.user.nick, m.messageString),
+    'ACTION': lambda m: f'*{m.user.nick} {m.messageString}*',
+    'NOTICE': lambda m: f'[{m.user.nick}] {m.messageString}',
+    'JOIN': lambda m: f'>> {m.user.nick} ({m.user.ident}@{m.user.host}) joined {m.replyTo}',
+    'NICK': lambda m: f'{m.user.nick} is now known as {m.messageString}',
+    'PART': lambda m: f'<< {m.user.nick} ({m.user.ident}@{m.user.host}) left {m.replyTo}{": " + m.messageString if m.messageString else ""}',
+    'QUIT': lambda m: f'<< {m.user.nick} ({m.user.ident}@{m.user.host}) quit{": " + m.messageString if m.messageString else ""}',
+    'KICK': lambda m: f'-- {m.metadata["kicked"]} was kicked by {m.user.nick}{": " + m.messageString if m.messageString else ""}',
+    'TOPIC': lambda m: f'-- {m.user.nick} set the topic to: {m.messageString}',
     'MODE': lambda m: formatMode(m),
 }
 
 logSelfFuncs = {
     ResponseType.Say: lambda bot, r: formatSelfPrivmsg(bot, r),
-    ResponseType.Do: lambda bot, r: '*{0} {1}*'.format(bot.nick, r.response),
-    ResponseType.Notice: lambda bot, r: '[{0}] {1}'.format(bot.nick, r.response),
+    ResponseType.Do: lambda bot, r: f'*{bot.nick} {r.response}*',
+    ResponseType.Notice: lambda bot, r: f'[{bot.nick}] {r.response}',
 }
 
 def formatSelfPrivmsg(bot, response):
@@ -42,7 +42,7 @@ def formatSelfPrivmsg(bot, response):
     else:
         status = ''
 
-    return '<{0}{1}> {2}'.format(status, bot.nick, response.response)
+    return f'<{status}{bot.nick}> {response.response}'
 
 def formatPrivmsg(msg: IRCMessage):
     if msg.targetType == TargetTypes.CHANNEL:
@@ -50,7 +50,7 @@ def formatPrivmsg(msg: IRCMessage):
     else:
         status = ''
 
-    return '<{0}{1}> {2}'.format(status, msg.user.nick, msg.messageString)
+    return f'<{status}{msg.user.nick}> {msg.messageString}'
 
 def formatMode(msg: IRCMessage):
     added = msg.metadata['added']
@@ -73,16 +73,16 @@ def formatMode(msg: IRCMessage):
         if len(removedParams) > 0:
             modeStr += ' {}'.format(' '.join(removedParams))
 
-    return '-- {} sets mode: {}'.format(msg.user.nick, modeStr)
+    return f'-- {msg.user.nick} sets mode: {modeStr}'
 
 
 def log(path, target, text):
     now = datetime.datetime.utcnow()
     time = now.strftime("[%H:%M:%S]")
-    data = '{0} {1}'.format(time, text)
+    data = f'{time} {text}'
     print(target, data)
 
-    fileName = "{0}{1}.txt".format(target, now.strftime("-%Y%m%d"))
+    fileName = f'{target}/{now.strftime("%Y%m%d")}.txt'
     fileDirs = path
     if not os.path.exists(fileDirs):
         os.makedirs(fileDirs)
