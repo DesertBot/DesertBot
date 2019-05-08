3d1b64a50360c368a906a2a56059707fdb033060
Heufneutje <stefan.frijters@gmail.com>
2019-05-08 17:57:53 +0000

    [Log] Third time's the charm

--- desertbot/modules/automatic/Log.py
+++ desertbot/modules/automatic/Log.py
@@ -151,7 +151,7 @@ class Log(BotCommand):
         return response
 
     def execute(self, message):
-        if message.type != TargetTypes.CHANNEL:
+        if message.targetType != TargetTypes.CHANNEL:
             return IRCResponse(ResponseType.Say, "I don't keep logs for private messages.", message.replyTo)
 
         basePath = self.bot.logpath
