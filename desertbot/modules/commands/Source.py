ad8fbf5a598953ddcf7f8fee089ca0b2ecaca85c
StarlitGhost <679547+StarlitGhost@users.noreply.github.com>
2019-07-16 01:50:06 +0100

    [Source] add the ability to lookup the source of commands/modules directly

--- desertbot/modules/commands/Source.py
+++ desertbot/modules/commands/Source.py
@@ -11,6 +11,9 @@ from zope.interface import implementer
 from desertbot.message import IRCMessage
 from desertbot.response import IRCResponse, ResponseType
 
+import inspect
+import os
+
 
 @implementer(IPlugin, IModule)
 class Source(BotCommand):
@@ -18,13 +21,36 @@ class Source(BotCommand):
         return ['source']
 
     def help(self, query):
-        return "source - returns a link to {0}'s source".format(self.bot.nick)
+        return (f"source (<command>) - returns a link to {self.bot.nick}'s source, "
+                f"or a specific command/module if given")
 
     def execute(self, message: IRCMessage):
-        sourceURL = self.bot.config.getWithDefault('source',
-                                                   'https://github.com/DesertBot/DesertBot/')
+        rootSourceURL = self.bot.config.getWithDefault('source',
+                                                       'https://github.com/DesertBot/DesertBot/')
+
+        if message.parameterList:
+            command = message.parameterList[0].lower()
+            mh = self.bot.moduleHandler
+            if command in mh.mappedTriggers:
+                module = mh.mappedTriggers[command].__class__
+            elif command in mh.caseMap:
+                module = mh.modules[mh.caseMap[command]].__class__
+            else:
+                return IRCResponse(ResponseType.Say,
+                                   f'"{command}" not recognized as a command or module name',
+                                   message.replyTo)
+            fullModulePath = inspect.getsourcefile(module)
+            relModulePath = os.path.relpath(fullModulePath)
+
+            fullModuleURL = f"{rootSourceURL}blob/master/{relModulePath}"
+            name = module.__name__
+
+            return IRCResponse(ResponseType.Say,
+                               f"Source of {name}: {fullModuleURL}",
+                               message.replyTo)
+
         return IRCResponse(ResponseType.Say,
-                           sourceURL,
+                           rootSourceURL,
                            message.replyTo)
 
 
