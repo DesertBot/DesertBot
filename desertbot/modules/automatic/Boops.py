7f90ad51414cace591aaa7d248cdaf9760f12012
HubbeKing <hubbe128@gmail.com>
2019-09-28 12:54:09 +0300

    Enable adding/removing boops with command during runtime

--- desertbot/modules/automatic/Boops.py
+++ desertbot/modules/automatic/Boops.py
@@ -1,5 +1,6 @@
 from twisted.plugin import IPlugin
 from desertbot.moduleinterface import IModule, BotModule, ignore
+from desertbot.modules.commandinterface import admin
 from zope.interface import implementer
 
 import random
@@ -18,14 +19,37 @@ class Boops(BotModule):
                                                ('action-channel', 1, self.respond),
                                                ('action-user', 1, self.respond)]
 
-    def help(self, arg):
-        return 'Responds to boops.'
+    def help(self, arg: list) -> str:
+        return f"Responds to boops. Admins may use {self.bot.commandChar}boop add/remove <url> to add and remove boops."
 
     @ignore
     def respond(self, message: IRCMessage) -> IRCResponse:
-        match = re.search('(^|[^\w])b[o0]{2,}ps?([^\w]|$)', message.messageString, re.IGNORECASE)
-        if match:
-            return IRCResponse(ResponseType.Say, f"Boop! {random.choice(self.bot.storage['boops'])}", message.replyTo)
+        # TODO store boops in self.bot.storage['boops'] as a dict of boopName to boopUrl, for easier identification in add/remove
+        if message.command == "boop":
+            subcommand = message.parameterList[0]
+            if subcommand == "add":
+                return self._addBoop(message)
+            elif subcommand == "remove":
+                return self._removeBoop(message)
+            else:
+                return IRCResponse(ResponseType.Say, self.help(message.parameterList), message.replyTo)
+        else:
+            match = re.search('(^|[^\w])b[o0]{2,}ps?([^\w]|$)', message.messageString, re.IGNORECASE)
+            if match:
+                return IRCResponse(ResponseType.Say, f"Boop! {random.choice(self.bot.storage['boops'])}", message.replyTo)
+
+    @admin("Only my admins may add boops!")
+    def _addBoop(self, message: IRCMessage) -> IRCResponse:
+        self.bot.storage['boops'].append(message.parameterList[1])
+        return IRCResponse(ResponseType.Say, f"Added {message.parameterList[1]} to the list of boops!", message.replyTo)
+
+    @admin("Only my admins may remove boops!")
+    def _removeBoop(self, message: IRCMessage) -> IRCResponse:
+        if message.parameterList[1] in self.bot.storage['boops']:
+            self.bot.storage['boops'].remove(message.parameterList[1])
+            return IRCResponse(ResponseType.Say, f"Removed {message.parameterList[1]} from the list of boops!", message.replyTo)
+        else:
+            return IRCResponse(ResponseType.Say, f"Couldn't find {message.parameterList[1]} in the list of boops, did you maybe do a typo?", message.replyTo)
 
 
 boop = Boops()
